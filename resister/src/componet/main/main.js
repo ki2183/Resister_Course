33059d4efc25169bc6643e14150e12453fbe8640
ki2183 <ki2183@naver.com>
2023-06-06 10:22:58 +0900

    app_0606

--- resister/src/componet/main/main.js
+++ resister/src/componet/main/main.js
@@ -1,49 +1,69 @@
 import { useState } from "react"
 import "./main.css"
+import { gsap } from "gsap"
+function Content(probs){
+
+    const e = probs.contentInfo
+
+    return (<div key={e.name} className="menu-content" onMouseLeave={probs.onLeave} onMouseEnter={probs.onEnter} onClick={event=>{
+        event.preventDefault()
+        window.location.href = e.link
+    }}>
+   
+        <div className="menu-content-num">
+            <p className="menu-content-number">{e.num}</p>
+            <p className="menu-content-p">{e.name}</p>
+        </div>
+        <div className="menu-content-intro">
+            <p className="menu-content-intro-p">{e.intro}</p>
+        </div>
+        <div className="menu-content-img">
+            <img src={e.imgurl} className="menu-content-img-icon"></img>
+        </div>
+    </div>)
+}
 
 function MainMenu(){
 
-    const [content,setContent] = useState([]) //메뉴 안에 컨텐츠들
-
     const contentInfo = [
         {
-            name : "저장된 시간표",
+            name : "시간표",
+            intro: "시간표만들기에서 저장했던시간표들을 불러오고 관리 할 수 있습니다.",
             imgurl : "",
-            link:"/"
+            link:"/",
+            num:"01"
         },
         {
             name : "정보 수정",
+            intro: "학년, 학번, 이름등 개인정보를 수정 할 수 있습니다.",
             imgurl : "",
-            link:"/"
+            link:"/",
+            num:"02"
         },
         {
             name : "시간표 만들기",
+            intro: "질문을 통해서 자동으로 시간표를 만들 수 있습니다.",
             imgurl : "",
-            link:"/"
+            link:"/",
+            num:"03"
         },
     ]
 
-    useState(()=>{
-        const content_ = []
 
-        contentInfo.forEach(e => {
-            content_.push(
-                <div key={e.name} className="menu-content" onClick={event=>{
-                    event.preventDefault()
-                    window.location.href = e.link
-                }}>
-                    <div className="menu-content-img">
-                        
-                    </div>
-                    <p className="menu-content-p">{e.name}</p>
-                </div>
-            )
-        });
-        setContent(content_)
-    },[])
+    const onEnter = ({ currentTarget }) => {
+        gsap.to(currentTarget, { scale: 1.05 });
+      };
+      
+      const onLeave = ({ currentTarget }) => {
+        gsap.to(currentTarget, { scale: 1 });
+      };
 
     return <div className="container-main-menu">
-      {content}
+
+        <Content onEnter={onEnter} onLeave={onLeave} contentInfo={contentInfo[0]} />
+        <Content onEnter={onEnter} onLeave={onLeave} contentInfo={contentInfo[1]} />
+        <Content onEnter={onEnter} onLeave={onLeave}contentInfo={contentInfo[2]} />
+
     </div>
 }
 
